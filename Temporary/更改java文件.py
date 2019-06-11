c58d464e7380fe837f207ff08b82c43c59da0896
[SLdeongaree] <@gmail.com>
2019-06-11 22:09:37 +0800

    Git of python is coming!

---
+++ Temporary/更改java文件.py
@@ -0,0 +1,22 @@
+import os
+import os.path
+
+
+def listdir(path, list_name):
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        if os.path.isdir(file_path):
+            listdir(file_path, list_name)
+        elif os.path.splitext(file_path)[1]=='.java':
+            list_name.append(file_path)
+
+allfile=[]
+def file_name(file_dir): 
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if os.path.splitext(file)[1]=='.java':
+                name=os.path.join(root, file)
+                allfile.append(name)
+
+onedir=r'E:\代码\云端同步\Java'
+file_name(onedir)
