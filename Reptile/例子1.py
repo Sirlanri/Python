75f12cd024223aca2be5147f3b146eaeabd43d85
[SLdeongaree] <@gmail.com>
2019-07-19 21:04:51 +0800

    终于能用了

--- Reptile/例子1.py
+++ Reptile/例子1.py
@@ -11,10 +11,23 @@ def mooc():
     print(titles)
 
 def tieba():
+    pics=[]
     url='http://tieba.baidu.com/p/6198229867?pn=1'
     web=urllib.request.urlopen(url).read()
     soup=BeautifulSoup(web,features="lxml")
+    print(soup.div.string)
     for link in soup.find_all('img'):
-        print(link.get('src'))
+        pics.append(link.get('src'))
     
-tieba()
+def blog1():
+    url='http://liangyj_blog.gitee.io/'
+    web=urllib.request.urlopen(url).read()
+    soup=BeautifulSoup(web,'lxml')
+    titles=soup.find_all('a','post-title-link')
+    for title in titles:
+        print(title.next)
+    
+    #after=re.findall('itemprop="url">\S</a>',title)
+    
+
+blog1()
