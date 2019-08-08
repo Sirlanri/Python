a51b15bcca0c34e3ede4e5d80c7319c3ace177e2
[SLdeongaree] <@gmail.com>
2019-08-08 12:08:25 +0800

    继续爬虫 脑子里有个想法了

--- Reptile/例子1.py
+++ Reptile/例子1.py
@@ -29,5 +29,17 @@ def blog1():
     
     #after=re.findall('itemprop="url">\S</a>',title)
     
+def douban():
+        total={}
+        users=[]
+        url='https://movie.douban.com/subject/24773958/comments?sort=new_score&status=P'
+        web=urllib.request.urlopen(url).read()
+        soup=BeautifulSoup(web,'lxml')
+        comments=soup.find_all('span',class_='short')
+        words=soup.find_all('span',class_='comment-info')
+        for word in words:
+                temp=word.select('a',class_='')
+                #users.append(re.findall(r'>\w</a>',temp))
+        print(users)
 
-blog1()
+douban()
--- Temporary/七夕.py
+++
@@ -1,9 +0,0 @@
-import random
-class Love:
-    '''It takes only a little courage to get rid of singles,
-     but looking for true love may be half-baked.'''
-
-    
-    def __init__(self):
-        
-    def do():
