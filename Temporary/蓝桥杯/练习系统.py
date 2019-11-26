9dffe2f1bcc69e7d43c2aa6725284b42160839bf
[SLdeongaree] <@gmail.com>
2019-11-26 20:10:51 +0800

    开始入坑算法比赛咯~

---
+++ Temporary/蓝桥杯/练习系统.py
@@ -0,0 +1,23 @@
+def fshulie(n):
+    n=int(input())
+    n1=n2=n3=1
+    for i in range(n-2):
+        n1,n2=n2,n3 #一股Python味儿~
+        n3=n2+n1
+    print(n3%10007)
+
+def two():
+    source=input()
+    list1=source.split()
+    print(int(list1[0])+int(list1[1]))
+
+#下面的是入门
+
+#杨辉三角
+
+n=int(input())
+lastline=[1,1]
+nextline=[1]
+for i in range(n):
+    for j in range(len(lastline)):
+        nextline.append(lastline[i-1]+lastline[i])
