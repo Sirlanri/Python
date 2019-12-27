2b83769ca1f0611ada5e0537f2370a8f2b247f7d
[SLdeongaree] <@gmail.com>
2019-12-27 15:40:37 +0800

    做了点完善

--- HomeWork/FinalExam/5.py
+++ HomeWork/FinalExam/5.py
@@ -1,9 +1,11 @@
 special = ['x', 'y', 'z','X','Y','Z']
 
 while 1:
-    getnum = input('input')
-    if getnum in special:
-        after = ord(getnum)-23
-    else:
-        after = ord(getnum)+3
-    print('after:{}'.format(chr(after)))
+    #输入单个字符串
+    getnums = list(input('input'))
+    for getnum in getnums:
+        if getnum in special:
+            after = ord(getnum)-23
+        else:
+            after = ord(getnum)+3
+        print('after:{}'.format(chr(after)))
