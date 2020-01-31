df1a0b80e197cd469bb04f2b61778831ebf4a64b
[SLdeongaree] <@gmail.com>
2020-01-31 10:57:52 +0800

    wdnmd算法，ヾ(￣▽￣)Bye~Bye~

--- Algorithm/simple/罗马数字转阿拉伯.py
+++ Algorithm/simple/罗马数字转阿拉伯.py
@@ -1,8 +1,49 @@
-import re
+luoma = input().lower()
+source = {'i': 1, 'v': 5, 'x': 10, 'l': 50, 'c': 100, 'd': 500, 'm': 1000, 'z':0}
+simbles = [item for item in luoma]
+second = []
+poplist = []
+length = len(simbles)
+for i in range(simbles.__len__()):
+    if i >= len(simbles)-1:
+        break
+    if simbles[i]+simbles[i+1] == 'iv':
+        second.append(4)
+        poplist.append(i)  # 把下标添加到另一个列表里
+        i+=1
 
-luoma=''
-source={'i':1,'v':5,'x':10,'l':50,'c':100,'d':500,'m':1000}
-simbles=luoma.split()
-second=[]
-for i in range(luoma.__len__):
-    if simbles[i]+
+    elif simbles[i]+simbles[i+1] == 'ix':
+        second.append(9)
+        poplist.append(i)
+        i+=1
+
+    elif simbles[i]+simbles[i+1] == 'xl':
+        poplist.append(i)
+        second.append(40)
+        i+=1
+
+    elif simbles[i]+simbles[i+1] == 'xc':
+        poplist.append(i)
+        second.append(90)
+        i+=1
+
+    elif simbles[i]+simbles[i+1] == 'cd':
+        poplist.append(i)
+        second.append(400)
+        i+=1
+
+    elif simbles[i]+simbles[i+1] == 'cm':
+        poplist.append(i)
+        second.append(900)
+        i+=1
+
+for index  in reversed(poplist):
+    simbles.pop(index)
+    simbles.pop(index+1)
+
+for num in simbles:
+    second.append(source[num])
+total = 0
+for num in second:
+    total += num
+print(total)
