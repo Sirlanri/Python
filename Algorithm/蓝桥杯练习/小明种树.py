fbacf1444d7110c000354ef1cbdf6b9e72533d59
深蓝 <mail@ri-co.cn>
2020-10-12 19:06:16 +0800

    这俩太难了，不会=-=

---
+++ Algorithm/蓝桥杯练习/小明种树.py
@@ -0,0 +1,17 @@
+#DFS
+
+treeList=[]
+def Rsum(x,y,r):
+    """
+    用半径和当面积
+    """
+    global treeList
+    #求出权值
+    
+    
+
+def getDistence(x,y,r1,r2):
+    """
+    通过坐标和半径求权值
+    """
+    
